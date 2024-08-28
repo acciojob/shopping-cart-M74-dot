50eebd14195dc7e2dc7f84e3f1da9a8730a993f8
Manisha Mali <82174253+M74-dot@users.noreply.github.com>
2024-08-28 22:18:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -38,7 +38,7 @@
             newRow.appendChild(qtyCell);
 
             const priceCell = document.createElement('td');
-            priceCell.textContent = `$${totalItemPrice.toFixed(2)}`;
+            priceCell.textContent = totalItemPrice;
             newRow.appendChild(priceCell);
 
             // Append the new row to the table
